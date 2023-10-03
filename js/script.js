207003e69ba27aaadc2f234e97ef27dd9bea2e08
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-03 13:07:08 +0200

    [edit]: contact

--- js/script.js
+++ js/script.js
@@ -5,16 +5,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      contacts
+      contacts,
+      indexActive: 0
     }
   },
   methods: {
+    lastItemArray(contact){
+      
+      return contact.messages[contact.messages.length - 1]
+
+    }
     
     
   },
   computed: {
+
   },
   mounted() {
+
   },
 
 }).mount('#app')
